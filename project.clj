652f197c1d3322c0e697702ee0c52f98eaf00018
David Goldfarb <deg@degel.com>
2017-12-12 19:45:30 +0200

    Update Clojure dep to 1.9.0 release

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :url "https://github.com/deg/sodium"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
                  [re-frame "0.10.2"]
                  [reagent "0.7.0"]
