9034ef0da984fcbf97988cb8103424dbe85f4789
David Goldfarb <deg@degel.com>
2018-04-15 11:35:08 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]
-                 [re-frame "0.10.2"]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [re-frame "0.10.5"]
                  [reagent "0.7.0"]
-                 [soda-ash "0.76.0"]
-                 [com.degel/iron "0.2.0"]]
+                 [soda-ash "0.78.2"]
+                 [com.degel/iron "0.3.0"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-doo "0.1.8"]]
   :cljsbuild
